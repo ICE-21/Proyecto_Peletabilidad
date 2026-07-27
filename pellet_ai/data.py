5b679f8b60b243c597f4d8ddb4cf3e5c6e760891
ICE-21 <75329508+ICE-21@users.noreply.github.com>
2026-07-26 23:41:54 -0500

    Update data.py

--- pellet_ai/data.py
+++ pellet_ai/data.py
@@ -4,6 +4,8 @@ Módulo encargado de leer y validar datos
 
 import pandas as pd
 
+from .config import DATA_PATH
+
 
 class DataManager:
 
@@ -11,24 +13,51 @@ class DataManager:
 
         self.df = None
 
-    def load_excel(self, path):
+
+    def load_excel(self, filename="lotes_peletizado.xlsx"):
+
+        """
+        Carga un archivo Excel desde la carpeta datos
+        """
+
+        path = DATA_PATH / filename
 
         self.df = pd.read_excel(path)
 
         return self.df
 
+
     def info(self):
+
+        """
+        Información general del dataframe
+        """
 
         return self.df.info()
 
+
     def describe(self):
+
+        """
+        Estadísticas descriptivas
+        """
 
         return self.df.describe()
 
+
     def null_values(self):
+
+        """
+        Conteo de valores nulos por columna
+        """
 
         return self.df.isnull().sum()
 
+
     def duplicated(self):
+
+        """
+        Número de registros duplicados
+        """
 
         return self.df.duplicated().sum()
